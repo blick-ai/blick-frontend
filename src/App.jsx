9523da8eab370f4f473c12e4a67ac5434181487d
Sakaef03 <fosakae@icloud.com>
2026-04-27 18:38:04 -0300

    feat(FRONT-002): cria tela de login

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css"
 import { BrowserRouter, Route, Routes } from "react-router";
 import Home from './pages/home';
+import Login from './pages/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   )
