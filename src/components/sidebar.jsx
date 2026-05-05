0c4b1edf5eaebd7cf67851db1a75c1c1216277c0
Sakaef03 <fosakae@icloud.com>
2026-05-05 14:14:38 -0300

    feat(FRONT-003): adiciona componentes finais do dashboard

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import { useNavigate } from "react-router"
 import CarInfo from "./carInfo"
+import CarRoute from "./carRoute"
 import Sweep from "./sweep"
 import Health from "./health"
 import logo from "../assets/images/logo-transparent.png"
@@ -32,13 +33,14 @@ export default function Sidebar() {
             )}
 
             <div className={`
-                fixed md:static top-0 left-0 z-50 h-full
+                fixed md:sticky md:top-0 top-0 left-0 z-50
+                h-screen
                 flex flex-col w-80 md:w-90 shrink-0
                 transition-transform duration-300 ease-in-out
                 ${open ? "translate-x-0" : "-translate-x-full"}
                 md:translate-x-0
             `}>
-                <div className="bg-[#1B2125] border border-[#8A898B]/25 text-[#8A898B] flex flex-row h-16 w-full py-3 px-3 justify-between items-center">
+                <div className="bg-[#1B2125] border border-[#8A898B]/25 text-[#8A898B] flex flex-row h-16 w-full py-3 px-3 justify-between items-center shrink-0">
                     <img src={logo} className="w-24 h-auto" />
                     <div className="flex flex-row gap-2 items-center">
                         <button
@@ -53,7 +55,6 @@ export default function Sidebar() {
                         </button>
                     </div>
                 </div>
-
                 <div className="bg-[#1B2125] border border-[#8A898B]/25 text-[#8A898B] flex flex-col flex-1 w-full py-3 px-3 gap-2 overflow-y-auto">
                     <p className="font-bold text-[10px]">CULTIVO MONITORADO</p>
                     <select className="bg-[#16191C] text-white text-xs border border-[#8A898B]/25 px-2 py-1.5 outline-none font-bold focus:border-[#4A9B9A] mb-1 w-full rounded-lg">
@@ -63,6 +64,8 @@ export default function Sidebar() {
                     </select>
                     <br />
                     <CarInfo battery={72} />
+                    <br />
+                    <CarRoute />
                     <br />
                     <Sweep imageNumber={8} leafNumber={139} affectedAreas={23} healthyQuantity={3} warningQuantity={3} criticalQuantity={2} />
                     <br />
