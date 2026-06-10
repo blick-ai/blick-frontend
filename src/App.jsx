6167e9d190d7a78546c21125b2176a4b43f5dd5c
Sakaef03 <fosakae@icloud.com>
2026-06-10 17:14:20 -0300

    fix(FRONT-004): corrige importação do componente protectedRoute

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Routes } from "react-router"
 import Home from './pages/home'
 import Login from './pages/login'
 import Dashboard from './pages/dashboard'
-import ProtectedRoute from './components/ProtectedRoute'
+import ProtectedRoute from './components/protectedRoute'
 
 function App() {
   return (
